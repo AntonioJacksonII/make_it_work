b8423744c7914510f54428fbd7759590d816e1a8
Antonio Jackson <antoniojackson@gmail.com>
2020-05-15 10:43:32 -0600

    Adding model test for the project_names method in the Contestant model

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -13,4 +13,18 @@ RSpec.describe Contestant, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:projects).through(:contestant_projects)}
   end
+
+  describe "project_names method" do
+    it "should list project names" do
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+
+      expect(gretchen.project_names).to eq("News Chic, Upholstery Tuxedo")
+    end
+  end
 end
