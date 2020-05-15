c1a7ebfa4718e07fc9be0c3a0246c610b1c3b4f0
Antonio Jackson <antoniojackson@gmail.com>
2020-05-15 10:29:12 -0600

    Adding model test for count method in the Projects model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,24 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe "count" do
+    it "can count the number of contestants" do
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
+      expect(news_chic.count).to eq(2)
+      expect(boardfit.count).to eq(1)
+    end
+  end
 end
