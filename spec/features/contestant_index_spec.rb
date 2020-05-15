cc0e1feca2717c3dbf57940286d07a254f836b40
Antonio Jackson <antoniojackson@gmail.com>
2020-05-15 09:55:46 -0600

    Adding User Story 2 functionality with tests

---
+++ spec/features/contestant_index_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe "Contestant Index Page" do
+  it "displays a list of all the names of the contestants" do
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+    visit "/contestants"
+
+    expect(page).to have_content("Jay McCarroll")
+    expect(page).to have_content("Gretchen Jones")
+  end
+
+  it "lists the contestant's projects under their name" do
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+
+    visit "/contestants"
+    expect(page).to have_content("Projects: News Chic")
+    expect(page).to have_content("Projects: News Chic Upholstery Tuxedo")
+  end
+end
